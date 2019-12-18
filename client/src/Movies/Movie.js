e18530cd51c1a4f5beab67ec333fa26069ff951e
nathan-saygers <nathan.saygers@gmail.com>
2019-12-18 13:46:10 -0800

    got the route up and running

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -31,6 +31,12 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  navToMovieForm = event => {
+    event.preventDefault();
+    console.log(event);
+    this.props.history.push(`/update-movie/${this.props.match.params.id}`)
+  }
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -41,6 +47,9 @@ export default class Movie extends React.Component {
         <MovieCard movie={this.state.movie} />
         <div className="save-button" onClick={this.saveMovie}>
           Save
+        </div>
+        <div className="edit-button" onClick={this.navToMovieForm}>
+          Edit
         </div>
       </div>
     );
---
+++ client/src/Movies/MovieForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieForm = props => {
+
+  return (
+    <form>
+
+    </form>
+  )
+}
+
+export default MovieForm;
